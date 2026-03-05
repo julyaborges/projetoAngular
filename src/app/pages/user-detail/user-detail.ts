42a0a9074e52ef99647aafa6ac580a0e713dc93f
Ana Julya Borges <borgesanajulya@gmail.com>
2026-03-05 11:28:22 -0300

    Botão de voltar

--- src/app/pages/user-detail/user-detail.ts
+++ src/app/pages/user-detail/user-detail.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user.interface';
 
 @Component({
   selector: 'app-user-detail',
+  imports: [RouterLink],
   templateUrl: './user-detail.html',
   styleUrl: './user-detail.css',
 })
@@ -13,17 +14,17 @@ export class UserDetail implements OnInit {
   usuario!: User
   usuarioNaoEncontrado: boolean = false;
 
-  constructor(private route: ActivatedRoute, private userService: UserService, private cdr:ChangeDetectorRef){}
+  constructor(private route: ActivatedRoute, private userService: UserService, private cdr:ChangeDetectorRef, private router: Router){}
 
   ngOnInit(){
     this.carregarUsuario();
   }
 
   carregarUsuario(){
-    const id = this.route.snapshot.paramMap.get('id');
+    const id = Number(this.route.snapshot.paramMap.get('id'));
 
       if(id){
-        this.userService.buscarUsuarioPorId(+id).subscribe({
+        this.userService.buscarUsuarioPorId(id).subscribe({
           next: (dados) => {
             if(!dados) {
               this.usuarioNaoEncontrado = true;
